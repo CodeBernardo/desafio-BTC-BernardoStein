cfa6b9f2738ebc98d1608bff1799e40470dc1696
CodeBernardo <stein.bernardo@proton.me>
2024-05-28 17:17:38 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RegisterAdressForm } from "./components/forms/registerAdress";
+
+function App() {
+  return (
+    <>
+      <RegisterAdressForm />
+    </>
+  );
+}
+
+export default App;
